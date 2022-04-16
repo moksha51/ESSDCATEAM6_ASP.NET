fee6cffc87744e04c344905457ffb2170ab531f5
moksha51 <xingjie.tan@gmail.com>
2022-04-16 17:35:51 +0800

    Added Nyein's sorting algo and added add to cart function button (commented out)

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -48,8 +48,9 @@ namespace CATeam6.Controllers
                 ViewData["cart"] = new List<Cart>(); //shows an empty cart
                 return View("Cart");
             }
-            else {
-                Guid userId = session.User.Id;  
+            else
+            {
+                Guid userId = session.User.Id;
                 if (userId != null)  //user has logged in; So show user's cart
                 {
                     cartItems = dbContext.Carts.Where(x => x.UserId.Id == userId).ToList();
@@ -57,7 +58,8 @@ namespace CATeam6.Controllers
                     string userCartAmt = cartItems.Sum(x => x.Quantity * x.Product.UnitPrice).ToString("#,0.00");
                     ViewData["userCartAmt"] = userCartAmt;
                 }
-                else { //user has not logged in; So show session's cart
+                else
+                { //user has not logged in; So show session's cart
                     cartItems = dbContext.Carts.Where(x => x.SessionId.Id == session.Id).ToList();
                     ViewData["cart"] = cartItems;
                     string userCartAmt = cartItems.Sum(x => x.Quantity * x.Product.UnitPrice).ToString("#,0.00");
@@ -93,7 +95,8 @@ namespace CATeam6.Controllers
 
                 userCartAmt = Math.Round(amt, 2).ToString("#,0.00");
             }
-            else { //user not logged in yet, so link cart to session instead
+            else
+            { //user not logged in yet, so link cart to session instead
                 Guid sessionId = session.Id;
                 newquantity = values.Quantity;
 
@@ -138,7 +141,8 @@ namespace CATeam6.Controllers
                 amt = dbContext.Carts.Where(x => x.SessionId.Id == session.Id).Sum(x => x.Quantity * x.Product.UnitPrice);
                 userCartAmt = Math.Round(amt, 2).ToString("#,0.00");
             }
-            else {
+            else
+            {
                 cartItem = dbContext.Carts.FirstOrDefault(x => x.UserId.Id == user.Id && x.Product.ProductId == item.ProductId);
                 dbContext.Remove(cartItem);
                 dbContext.SaveChanges();
@@ -167,5 +171,27 @@ namespace CATeam6.Controllers
 
             return session;
         }
+
+        //public ActionResult Add(Mobiles mo)
+        //{
+        //    if (Session["cart"] == null)
+        //    {
+        //        List<Mobiles> li = new List<Mobiles>();
+        //        li.Add(mo);
+        //        Session["cart"] = li;
+        //        ViewBag.cart = li.Count();
+        //        Session["count"] = 1;
+        //    }
+        //    else
+        //    {
+        //        List<Mobiles> li = (List<Mobiles>)Session["cart"];
+        //        li.Add(mo);
+        //        Session["cart"] = li;
+        //        ViewBag.cart = li.Count();
+        //        Session["count"] = Convert.ToInt32(Session["count"]) + 1;
+
+        //    }
+        //    return RedirectToAction("Index", "Home");
+        //}
     }
 }
